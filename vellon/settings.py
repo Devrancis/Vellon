85f0ba795e72784813f6a0a069fb9daec8aa509b
Devrancis <francisiyiola.fi@gmail.com>
2026-05-03 20:03:46 +0100

    refactor: reconfigure allowed hosts

--- vellon/settings.py
+++ vellon/settings.py
@@ -16,10 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-test-key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
